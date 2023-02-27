b1ba0b9e503244d7fada5f2da18c4df2c87eb528
Patrick Arns <git@patrick-arns.de>
2023-02-27 18:52:39 +0100

    Added Vercel analytics

--- src/layouts/main/index.jsx
+++ src/layouts/main/index.jsx
@@ -1,9 +1,12 @@
 import * as React from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 const MainLayout = ({ children }) => {
   return (
     <div>
       {children}
+
+      <Analytics />
     </div>
   );
 };
