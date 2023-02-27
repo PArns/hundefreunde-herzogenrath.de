3625db180e4bc12dcfdc86fb889d7a4022d84ad7
Patrick Arns <git@patrick-arns.de>
2023-02-27 18:08:53 +0100

    Moved to JSX

---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+
+const IndexPage = () => {
+  return (
+    <h1>Hello World!</h1>
+  )
+}
+
+export default IndexPage
+
+export const Head = () => <title>Home Page</title>
