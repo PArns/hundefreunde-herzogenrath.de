37dce986e5e856c9946aca2275583a94e522eda4
Patrick Arns <git@patrick-arns.de>
2023-04-02 00:11:22 +0200

    footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,4 +1,7 @@
 import * as React from "react";
+import { MapPinIcon, AtSymbolIcon } from "@heroicons/react/20/solid";
+import Obfuscate from "react-obfuscate";
+import { Link } from "gatsby";
 
 const CurrentYear = () => {
   return <>{new Date().getFullYear()}</>;
@@ -8,15 +11,39 @@ const Footer = () => {
   return (
     <div className="flex w-full bg-gray-800 text-white">
       <div className="container mx-auto">
-        <div className="flex flex-col justify-between py-4 md:flex-row">
+        <div className="flex flex-col justify-between pt-4 md:flex-row">
           <div className="flex flex-col">
             <div className="text-xl font-bold">
               Hundefreunde Herzogenrath e.V.
             </div>
-            <div className="text-sm">Die Hundeschule im Kreis Aachen</div>
+            <div className="mb-2 text-sm">Die Hundeschule im Kreis Aachen</div>
+            <div className="flex text-sm">
+              <MapPinIcon className="mr-1 w-4" /> Grenzstr. 9 - 52134
+              Herzogenrath
+            </div>
+            <div className="flex text-sm">
+              <AtSymbolIcon className="mr-1 w-4" />{" "}
+              <Obfuscate email={"info@hundefreunde-herzogenrath.de"} />
+            </div>
+          </div>
+          <div className="mx-auto flex flex-col">
+            <div className="text-md">
+              <div>
+                <Link to="/kurse/#main">Kurse</Link>
+              </div>
+              <div>
+                <Link to="/termine/#main">Termine</Link>
+              </div>
+              <div>
+                <Link to="/team/#main">Team</Link>
+              </div>
+              <div>
+                <Link to="/anfahrt/#main">Impressum</Link>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="my-4 border-t border-gray-500 pt-4 text-center text-gray-500">
+        <div className="my-2 border-t border-gray-500 pt-2 text-center text-gray-500">
           © <CurrentYear /> Hundefreunde Herzogenrath e.V.
         </div>
       </div>
